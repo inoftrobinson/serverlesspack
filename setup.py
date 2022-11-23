f45f0de39136399f379fea6e6549ef8bea304034
Robinson Labourdette <robinson@inoft.com>
2022-11-23 17:40:30 +0100

    Added tqdm to install_requires & released 0.4.4

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="serverlesspack",
-    version="0.4.3",
+    version="0.4.4",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "PyYAML", "pydantic", "boto3", "distlib", "importlib-metadata", "colorama", "asciitree"],
+    install_requires=["click", "PyYAML", "pydantic", "boto3", "distlib", "importlib-metadata", "colorama", "asciitree", "tqdm"],
     url="https://github.com/Robinson04/serverlesspack",
     license="MIT",
     author="Inoft",
